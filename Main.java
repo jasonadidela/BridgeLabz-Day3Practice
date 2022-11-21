a0aeab9259d73f4b65927369a004e12f9f0d8588
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:45:16 +0530

    Final code for 3.2_FrequencyInArray Branch is created.

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+//Java Program to find the frequency of each element in the array
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
-        System.out.println("Welcome to 3.2_FrequencyInArray Branch");
+        int [] arr = new int [] {1, 2, 3, 4, 5};
+        int [] fr = new int [arr.length];
+        int visited = -1;
+        for(int i = 0; i < arr.length; i++){
+            int count = 1;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                    fr[j] = visited;
+                }
+            }
+            if(fr[i] != visited)
+                fr[i] = count;
+        }
+        System.out.println(" Element | Frequency");
+        for(int i = 0; i < fr.length; i++){
+            if(fr[i] != visited)
+                System.out.println("    " + arr[i] + "    |    " + fr[i]);
+        }
     }
 }

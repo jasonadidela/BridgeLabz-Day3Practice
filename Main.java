ec131f8b3e5c3d05de073aeb666fe5be0458b7a3
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:34:58 +0530

    Final code for 3.9_AscendArray Branch is created.

--- Main.java
+++ Main.java
@@ -1,6 +1,33 @@
+//9) Java Program to sort the elements of an array in ascending order
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
-        System.out.println("Welcome to 3.9_AscendArray Branch");
+        Scanner value=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+        int a = value.nextInt();
+        int[] ary = new int[a];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<a; i++) {
+            ary[i]=value.nextInt();
+        }
+        for (int k : ary) {
+            System.out.print(k + " ");
+        }
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = i+1; j < ary.length; j++) {
+                if(ary[i] > ary[j]) {
+                    int temp = ary[i];
+                    ary[i] = ary[j];
+                    ary[j] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Elements of array sorted in ascending order: ");
+        for (int j : ary) {
+            System.out.print(j + " ");
+        }
     }
 }

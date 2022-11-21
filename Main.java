62962297d972f975a95c80fadabf370584770778
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:43:57 +0530

    Final code for 3.3_LargestInArray Branch is created.

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+//3) Java Program to print the largest element in an array
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
-        System.out.println("Welcome to 3.3_LargestInArray Branch");
-
+        Scanner value = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+        int n=value.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++){
+            array[i]=value.nextInt();
+        }
+        int max = array[0];
+        for (int j : array) {
+            if (j > max)
+                max = j;
+        }
+        System.out.println("Largest element present in given array: " + max);
     }
 }

393fa26addc70bcb24898ae1b008e6c0de83ee4b
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:36:57 +0530

    Final code for 3.8_DuplicateInArray Branch is created.

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+////8) Java Program to print the duplicate elements of an array
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
-        System.out.println("Welcome to 3.8_DuplicateInArray Branch");
+        int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};
+        System.out.println("Duplicate elements in given array: ");
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++)
+                if (arr[i] == arr[j])
+                    System.out.println(arr[j]);
+        }
     }
 }

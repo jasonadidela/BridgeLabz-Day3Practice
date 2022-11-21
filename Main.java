474f0750069a6b6dc9b2b8eb6a41029465e7bdc9
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:39:46 +0530

    Final code for 3.6_ReverseArray Branch is created.

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+//6) Java Program to print the elements of an array in reverse order
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
-        System.out.println("Welcome to 3.6_ReverseArray Branch");
+        Scanner value=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+        int a = value.nextInt();
+        int[] ary = new int[a];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<a; i++) {
+            ary[i]=value.nextInt();
+        }
+        System.out.println("Original array: ");
+        for (int j : ary) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println("Array in reverse order: ");
+        for (int i = ary.length-1; i >= 0; i--) {
+            System.out.print(ary[i] + " ");
+        }
     }
 }

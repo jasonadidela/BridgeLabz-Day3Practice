dd00ea34f80523c3d654cd53cadac8c0e58a2336
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:21:32 +0530

    New 3.2_FrequencyInArray Branch is created first commit.

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
+        System.out.println("Welcome to 3.2_FrequencyInArray Branch");
     }
 }

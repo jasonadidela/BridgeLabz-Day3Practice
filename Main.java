f386af030bac00812efd8bfcc954f254ec8462ce
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:46:45 +0530

    Final code for 3.1_DisplayElementsInArray Branch is created.

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+//1) Java Program to print the elements of an array
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day3Practice Main Branch");
-        System.out.println("Welcome to 3.1_DisplayElementsInArray Branch");
+        int n;
+        Scanner var=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+        n=var.nextInt();
+        int[] array = new int[10];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++) {
+            array[i]=var.nextInt();
+        }
+        System.out.println("Array elements are: ");
+        for (int i=0; i<n; i++){
+            System.out.println(array[i]);
+        }
     }
 }
